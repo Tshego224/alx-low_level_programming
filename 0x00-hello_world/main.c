b85e2eaccc1598839cfa68a59f45be181003fcd4
Tshego224 <tshegomotswiri@gmail.com>
2023-10-18 19:17:51 -0700

    100-intel

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,28 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 /**
- * main - generating the assembly code(Intel Syntax) of a C code and saving it in an output file
+ * main - Entry point
  *
- * Return: 0
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	char *CFILE = "main.c";
-	char *filename = strdup(CFILE);
-	char *basename = strtok(filename, ".");
-	char command[100];
-	sprintf(command, "gcc -S -masm=intel %s -o %s.s", CFILE, basename);
-	int result = system(command);
-	if (result == 0) {
-        printf("Assembly code generated successfully. Check '%s.s'.\n", basename);
-	}
-       	else {
-        printf("Error: Failed to generate assembly code.\n");
-    }
-	free(basename);
 
     return 0;
 }
