2ae5449204d7573128e4f611a91fed7104825083
Tshego224 <tshegomotswiri@gmail.com>
2023-10-18 19:27:30 -0700

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
  * main - A  program that prints a line to the standard error
  * Return: 1
@@ -7,6 +8,7 @@
 
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+
+	return (1);
 }
